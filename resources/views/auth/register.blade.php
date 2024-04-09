b66eacdb55aaae893330b6761b8091ce30b02bab
kholoud001 <kholoud.sanak@gmail.com>
2024-04-09 11:48:12 +0000

    register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form id="registerForm">
                         @csrf
 
                         <div class="row mb-3">
@@ -75,3 +75,4 @@
     </div>
 </div>
 @endsection
+
